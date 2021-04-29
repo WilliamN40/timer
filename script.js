ad58f451d05482ae7bef49644c22e39b8f064c1d
WilliamN40 <70229064+WilliamN40@users.noreply.github.com>
2021-04-29 14:25:37 +0700

    Use Clear Interval

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var interval
+
 document.addEventListener("DOMContentLoaded", () => {
     const input = document.querySelector("#time");
     const button = document.querySelector("#change");
@@ -5,9 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const today = new Date();
     
     button.addEventListener("click", () => {
+        clearInterval(interval);
+
         var time = new Date(today.getFullYear(), today.getMonth(), today.getDate(), input.value.slice(0,2), input.value.slice(3,5));
 
-        setInterval(() => {
+        interval = setInterval(() => {
             var today = new Date();
             h1.innerHTML = (time - today)/1000
         },50)
